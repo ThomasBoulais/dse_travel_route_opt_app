655dc7dc1b86e0fd831daf36bf44b94cabf09bc4
tboulais <tboulais@protonmail.com>
2026-03-03 23:45:13 +0100

    feat: add merge & next steps in exp3

--- testing_grounds/exp3.py
+++ testing_grounds/exp3.py
@@ -3,7 +3,7 @@ import fastparquet
 import geopandas as gpd
 
 from testing_grounds.exp2 import to_geopandas
-from travel_route_optimization.data_pipeline.utils.config import DT_DICT_TYPE_CAT, DT_SILVER_GEOPARQUET
+from travel_route_optimization.data_pipeline.utils.config import DT_DICT_TYPE_CAT, DT_SILVER_GEOPARQUET, OSM_SILVER_GEOPARQUET
 
 # DATATOURISME
 
@@ -24,12 +24,52 @@ def add_category(gdf: gpd.GeoDataFrame) -> gpd.GeoDataFrame:
     return gdf
 
 
-df_dt = fastparquet.ParquetFile(DT_SILVER_GEOPARQUET)
-df_dt = df_dt.to_pandas()
-gdf_dt = to_geopandas(df_dt)
+dt_df = fastparquet.ParquetFile(DT_SILVER_GEOPARQUET)
+dt_df = dt_df.to_pandas()
+dt_gdf = to_geopandas(dt_df)
 
-for i, row in add_category(gdf_dt)[['name_fr', 'types', 'categories']].head(50).iterrows():
-    print(row['name_fr'], row['categories'], '\n', row['types'], '\n\n')
+# for i, row in add_category(gdt_df)[['name_fr', 'types', 'categories']].head(50).iterrows():
+#     print(row['name_fr'], row['categories'], '\n', row['types'], '\n\n')
 
 # reste à créer la strctuure standardisée pour toutes les données
 
+osm_df = fastparquet.ParquetFile(OSM_SILVER_GEOPARQUET)
+osm_df = osm_df.to_pandas()
+osm_gdf = to_geopandas(osm_df)
+
+print('\nDT\n', dt_gdf.notnull().sum())
+print('\nOSM\n', osm_gdf.notnull().sum())
+
+# print(osm_gdf[osm_gdf['natural'].notnull()].head(10))
+
+
+
+# Re-projection métrique en CRS (Coordinate Reference System) pour le calcul des distances
+dt_gdf.set_crs("EPSG:2154", inplace=True)
+osm_gdf.set_crs("EPSG:2154", inplace=True)
+
+dt_m = dt_gdf.to_crs("EPSG:2154") # EPSG:2154 => projection métrique française officielle
+osm_m = osm_gdf.to_crs("EPSG:2154")
+
+merged = gpd.sjoin_nearest(
+    dt_m,
+    osm_m,
+    how = 'left', # les données DT sont enrichies des données OSM si elles existent
+    max_distance = 5, # en mètres
+    distance_col = "match_distance"
+)
+
+print('\n',merged[merged['id_right'].isnull()].notnull().sum())
+
+merged = gpd.sjoin_nearest(
+    dt_m,
+    osm_m,
+    how = 'right', # les données DT sont enrichies des données OSM si elles existent
+    max_distance = 5, # en mètres
+    distance_col = "match_distance"
+)
+
+print('\n',merged[merged['id_left'].isnull()].notnull().sum())
+# voir option pour merge (how = 'full outer' inexistant) maybe en faisant un écart et concat ?
+
+# name + geometry + category + opening hours + website + phone + address
